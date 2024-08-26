8f1c9e1c89c8149dd0d6c4bebf5f1d923a413313
Giulia Barbizan <giuliabarbizan@gmail.com>
2024-08-26 09:05:24 -0300

    criando as rotas e os componentes home, aluno e error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+/*DEPENDÊNCIAS PARA CRIAR AS ROTAS*/
 import App from './App.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Error from './routes/Error.jsx'
+{/*CRIANDO A FUNÇÃO CREATEBROWSERROTER*/}
 
+const router = createBrowserRouter([
+  {
+    /*CHAMANDO OS ELEMENTOS PAI*/
+    path:'/', element:<App/>,
+    errorElement:<Error/>,
+
+    //CHAMNADO OS ELEMENTOS FILHOS
+    children:[
+      //Criando a ROta Home
+      {path: '/', element:<Home/>},
+      //Criando a rota Aluno
+      {path: '/aluno', element: <Aluno/>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*Criando o props que será renderizado*/}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/routes/Error.jsx
@@ -0,0 +1,9 @@
+const Error =()=>{
+    return(
+        <>
+        <h1>404 - Página não Encontrada</h1>
+        </>
+    )
+}
+
+export default Error
